7ba9114745042976bb564c43d1d4dd40e158ef23
Duy Khuong <nnkhuongduy@gmail.com>
2021-09-20 18:05:00 +0700

    adding lib to batch graphql requests

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,23 +1,27 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { InMemoryCache } from '@apollo/client/core';
+import {
+  HttpBatchLink,
+  HttpBatchLinkModule,
+} from 'apollo-angular-link-http-batch';
 
 const uri = 'https://axieinfinity.com/graphql-server-v2/graphql'; // <-- add the URL of the GraphQL server here
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  return {
-    link: httpLink.create({uri}),
-    cache: new InMemoryCache(),
-  };
-}
+// export function createApollo(httpLink: HttpBatchLink) {
+//   return {
+//     link: httpLink.create({ uri, batchMax: 20 }),
+//     cache: new InMemoryCache(),
+//   };
+// }
 
 @NgModule({
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: createApollo,
-      deps: [HttpLink],
-    },
-  ],
+  imports: [HttpBatchLinkModule],
 })
-export class GraphQLModule {}
+export class GraphQLModule {
+  constructor(apollo: Apollo, httpLink: HttpBatchLink) {
+    apollo.create({
+      link: httpLink.create({ uri, batchMax: 40 }) as any,
+      cache: new InMemoryCache(),
+    });
+  }
+}
