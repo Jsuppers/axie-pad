a5029d1cc8b22690b95819af40d183b893f7710e
Duy Khuong <nnkhuongduy@gmail.com>
2021-10-01 21:15:10 +0700

    adding parts filter feature

--- src/app/components/dashboard/tables/finder/finder.component.ts
+++ src/app/components/dashboard/tables/finder/finder.component.ts
@@ -2,9 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { combineLatest, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { lowerCase } from 'lodash';
+import { FormBuilder } from '@angular/forms';
 
 import { UserService } from 'src/app/services/user/user.service';
 import { Axie } from 'src/app/_models/axie';
+import { AxiePart } from 'src/app/_models/part';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-finder',
@@ -13,15 +16,17 @@ import { Axie } from 'src/app/_models/axie';
 })
 export class FinderComponent implements OnInit {
   _axies: Axie[] = [];
+  _parts: AxiePart[] = [];
 
-  axies: Axie[] = []; // Displaying axies
   searchQuery: string = '';
+  partQuery: string = '';
+  partSelected: AxiePart;
 
   @Input()
   hideAddress$: Observable<boolean>;
   hideAddresses: boolean;
 
-  constructor(private user: UserService) {}
+  constructor(private fb: FormBuilder, private user: UserService) {}
 
   ngOnInit(): void {
     this.user
@@ -51,13 +56,47 @@ export class FinderComponent implements OnInit {
       )
       .subscribe((axies) => {
         this._axies = axies;
-        this.axies = this._axies;
-        this.searchQuery = '';
+        this._parts = axies.reduce((parts, currentAxie) => {
+          currentAxie.parts.forEach((part) => {
+            const alreadyAdded = Boolean(
+              parts.find((_part) => _part.name === part.name)
+            );
+
+            if (!alreadyAdded) {
+              parts.push(part);
+            }
+          });
+
+          return parts;
+        }, []);
+        this.onClearFilter();
       });
 
     this.hideAddress$.subscribe((hideAddresses) => {
       this.hideAddresses = hideAddresses;
     });
+  }
+
+  get filteredParts(): AxiePart[] {
+    return this._parts.filter((part) =>
+      lowerCase(part.name).includes(lowerCase(this.partQuery))
+    );
+  }
+
+  get filteredAxies(): Axie[] {
+    return this._axies
+      .filter((axie) =>
+        lowerCase(axie.name).includes(lowerCase(this.searchQuery))
+      )
+      .filter((axie) => {
+        if (!this.partSelected) {
+          return true;
+        }
+
+        return Boolean(
+          axie.parts.find((part) => part.name === this.partSelected.name)
+        );
+      });
   }
 
   getAxieClassColor(axieClass: string) {
@@ -80,9 +119,17 @@ export class FinderComponent implements OnInit {
     }
   }
 
-  onSearchAxies() {
-    this.axies = this._axies.filter((axie) =>
-      lowerCase(axie.name).includes(lowerCase(this.searchQuery))
-    );
+  displayFn(part: AxiePart): string {
+    return part && part.name ? part.name : '';
+  }
+
+  onSelectPart(event: MatAutocompleteSelectedEvent) {
+    this.partSelected = event.option.value;
+  }
+
+  onClearFilter() {
+    this.partSelected = undefined;
+    this.searchQuery = '';
+    this.partQuery = '';
   }
 }
