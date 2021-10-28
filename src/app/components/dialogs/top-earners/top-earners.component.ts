94c8aa6ac6858152869344caa09c3dfef4b7d470
Duy Khuong <nnkhuongduy@gmail.com>
2021-10-28 20:51:46 +0700

    updating top earners dialog to use table

--- src/app/components/dialogs/top-earners/top-earners.component.ts
+++ src/app/components/dialogs/top-earners/top-earners.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { UserService } from 'src/app/services/user/user.service';
-import { LeaderboardDetails } from 'src/app/_models/leaderboard';
-import { FirestoreScholar } from 'src/app/_models/scholar';
 import { SLP } from 'src/app/_models/slp';
-import { chunk } from 'lodash';
-import { PageEvent } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 enum ViewMode {
   slp,
@@ -17,14 +15,16 @@ enum ViewMode {
 
 class TableData {
   name: string;
-  average: number;
-  elo: number
+  averageSLP: number;
+  averageUSD: number;
+  elo: number;
+  winRate: number;
 }
 
 @Component({
   selector: 'app-top-earners',
   templateUrl: './top-earners.component.html',
-  styleUrls: ['./top-earners.component.scss']
+  styleUrls: ['./top-earners.component.scss'],
 })
 export class TopEarnersComponent implements OnInit {
   scholars: TableData[] = [];
@@ -32,53 +32,71 @@ export class TopEarnersComponent implements OnInit {
   viewMode = ViewMode;
 
   pageEvent: PageEvent;
+  dataSource = new MatTableDataSource<TableData>([]);
+  displayedColumns = [];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(
     public dialogRef: MatDialogRef<TopEarnersComponent>,
-    private userService: UserService,
-  ) { }
+    private userService: UserService
+  ) {}
 
-
-    async ngOnInit(): Promise<void> {
-      combineLatest([
+  async ngOnInit(): Promise<void> {
+    combineLatest([
       this.userService.getScholars(),
       this.viewMode$,
-    ]).pipe(switchMap(([scholars, viewMode]) => {
-      let output: Observable<TableData>[] = [];
-        scholars.forEach((scholar) => {
-          output.push(
-            combineLatest([
-              this.userService.getScholarsLeaderboardDetails(scholar.id),
-              this.userService.getScholarsSLP(scholar.id),
-            ]).pipe(
+      this.userService.getSLPPrice(),
+    ])
+      .pipe(
+        switchMap(([scholars, viewMode, slpPrice]) => {
+          let output: Observable<TableData>[] = [];
+          scholars.forEach((scholar) => {
+            output.push(
+              combineLatest([
+                this.userService.getScholarsLeaderboardDetails(scholar.id),
+                this.userService.getScholarsSLP(scholar.id),
+              ]).pipe(
                 map(([details, slp]) => {
+                  const winRate =
+                    (details.wins /
+                      (details.wins + details.loses + details.draws)) *
+                    100;
+                  const averageSLP = this.getAverageSLP(slp);
+                  const averageUSD = averageSLP * slpPrice;
+
                   const tableData: TableData = {
                     name: scholar.name,
-                    average: this.getAverageSLP(slp),
+                    averageSLP,
+                    averageUSD,
                     elo: details?.elo ?? 0,
+                    winRate: isNaN(winRate) ? 0 : winRate,
                   };
                   return tableData;
-              })
-              ),
+                })
+              )
             );
-        });
-        return combineLatest(output);
-    }),
-    withLatestFrom(this.viewMode$),
-    )
-    .subscribe(([tableData, viewMode]) => {
-        this.scholars = tableData;
+          });
+          return combineLatest(output);
+        }),
+        withLatestFrom(this.viewMode$)
+      )
+      .subscribe(([tableData, viewMode]) => {
         if (viewMode === ViewMode.slp) {
-          this.scholars.sort((a, b) => b.average - a.average);
+          this.displayedColumns = ['name', 'averageSLP', 'averageUSD'];
+          this.dataSource.data = tableData.sort(
+            (a, b) => b.averageSLP - a.averageSLP
+          );
         }
         if (viewMode === ViewMode.elo) {
-          this.scholars.sort((a, b) => b.elo - a.elo);
+          this.displayedColumns = ['name', 'elo', 'winRate'];
+          this.dataSource.data = tableData.sort((a, b) => b.elo - a.elo);
         }
       });
-    }
+  }
 
-  convertToTableData(scholar: FirestoreScholar): Observable<LeaderboardDetails> {
-    return this.userService.getScholarsLeaderboardDetails(scholar.id);
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   getAverageSLP(slp: SLP, dateNow: Date = new Date()): number {
@@ -88,12 +106,14 @@ export class TopEarnersComponent implements OnInit {
     const inProgressSLP = slp.inProgress;
     const dateClaimed: Date = new Date(slp.lastClaimed * 1000);
 
-    const seconds = Math.floor((dateNow.getTime() - dateClaimed.getTime()) / 1000);
+    const seconds = Math.floor(
+      (dateNow.getTime() - dateClaimed.getTime()) / 1000
+    );
     const secondsInDay = 86400;
     if (seconds < secondsInDay) {
       return inProgressSLP;
     }
-    return (inProgressSLP / seconds * secondsInDay);
+    return (inProgressSLP / seconds) * secondsInDay;
   }
 
   close(): void {
@@ -101,15 +121,7 @@ export class TopEarnersComponent implements OnInit {
   }
 
   sortScholars(): void {
-    this.scholars.reverse();
-  }
-
-  get scholarChunks() {
-    if (this.pageEvent) {
-      return chunk(this.scholars, this.pageEvent.pageSize)[this.pageEvent.pageIndex];
-    } else {
-      return chunk(this.scholars, 10)[0];
-    }
+    this.dataSource.data = this.dataSource.data.reverse();
   }
 
   onTabChange(index: number) {
@@ -118,5 +130,19 @@ export class TopEarnersComponent implements OnInit {
     } else {
       this.viewMode$.next(ViewMode.elo);
     }
+  }
+
+  getAverageChipColor(averageSLP: number): string {
+    const colors = this.userService.currentColors();
+    if (Math.round(averageSLP) < colors[0]) {
+      return '#FF0000'; // red
+    }
+    if (Math.round(averageSLP) < colors[1]) {
+      return '#FF8000'; // orange
+    }
+    if (Math.round(averageSLP) < colors[2]) {
+      return '#00CC00'; // green
+    }
+    return '#FF00FF'; // pink
   }
 }
